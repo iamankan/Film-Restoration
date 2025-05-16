4f0260807c6bf54c071cd031d7100a517d67840a
Ankan Bhattacharyya <ankan.bhattacharyya.94@gmail.com>
2025-05-16 13:36:28 -0400

    Make a PCA script to run across layers and get the principle component images.

---
+++ data/scripts/python/pca.py
@@ -0,0 +1,55 @@
+import argparse
+from pathlib import Path
+import imageio.v3 as iio
+import numpy as np
+from sklearn.decomposition import PCA
+from einops import rearrange
+from natsort import natsorted
+
+def __pca__(input: Path, output: Path) -> None:
+    images = natsorted([i for i in input.iterdir()])
+    print(f"Input images: {images}")
+
+    # Step 1: Read images into numpy array
+    np_images = np.array([iio.imread(image) for image in images])
+    print(f"Original shape: {np_images.shape}")  # (n, h, w)
+
+
+    # Step 3: Flatten each image
+    n, h, w = np_images.shape
+    np_images_flatten = rearrange(np_images, 'c h w -> (h w) c')  # shape: (n, h*w)
+
+    # Step 4: PCA
+    pca = PCA(n_components=n)
+    pca_transform = pca.fit_transform(np_images_flatten)
+    # pca_transform = pca_fit.transform(np_images_flatten)
+
+    pca_transform = rearrange(pca_transform, '(h w) c -> c h w', c=n, h=h, w=w)
+
+    pca_transform = pca_transform.astype(np.float32)
+
+    output.mkdir(parents=True, exist_ok=True)
+
+    [iio.imwrite(f'{output}/pca_{i}.tif', x) for i,x in enumerate(pca_transform)]
+    
+
+
+
+def main():
+    """
+    This function is the main function that takes in no arguments.
+    """
+    parser = argparse.ArgumentParser(description='This is the main function. From this function it takes some arguments to calculate and give back pca.')
+    parser.add_argument('-i', '--input', help='Directory of the input images.', type=Path, required=True)
+    parser.add_argument('-o', '--output', help='Destination folder where the pca files will be stored.', required=True, type=Path)
+    # parser.add_argument('-n','--number-of-components', help='Number of principle components required. If not provided, it will take the number of images by default.', required=False, type=int)
+    args = parser.parse_args()
+    input = args.input
+    output = args.output
+    # num = args.number_of_components
+    __pca__(input=input, output=output)
+
+
+
+if __name__ == '__main__':
+    main()
