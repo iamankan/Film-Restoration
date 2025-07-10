6d5bb997dedd1c552ff220729252f0c0b63b1ecb
Ankan Bhattacharyya <ankan.bhattacharyya.94@gmail.com>
2025-07-10 10:42:34 -0400

    Updated code for dataset creation and registration, in batch.

--- data/scripts/register.sh
+++ data/scripts/register.sh
@@ -28,4 +28,8 @@ while [[ "$#" -gt 0 ]]; do
     esac
 done
 
-/Volumes/Working_4TB/utils/registration-toolkit/build/bin/rt_register -m /Volumes/Ankan_PhD/IlFord/EduceMount/Xometry_MJF_VP/FrameAvg/optical/${ID}.jpg -f /Volumes/Ankan_PhD/IlFord/EduceMount/Xometry_MJF_VP/FrameAvg/xray_render/edited/tif/${ID}_render.tif -o /Volumes/Ankan_PhD/IlFord/EduceMount/Xometry_MJF_VP/FrameAvg/registered/${ID}_reg.tif
+# /Volumes/Working_4TB/utils/registration-toolkit/build/bin/rt_register -m /Volumes/Ankan_PhD/IlFord/EduceMount/Xometry_MJF_VP/FrameAvg/optical/${ID}.jpg -f /Volumes/Ankan_PhD/IlFord/EduceMount/Xometry_MJF_VP/FrameAvg/xray_render/edited/tif/${ID}_render.tif -o /Volumes/Ankan_PhD/IlFord/EduceMount/Xometry_MJF_VP/FrameAvg/registered/${ID}_reg.tif
+echo "Registering for $ID (frangi+gaussian3)"
+/Volumes/Working_4TB/utils/registration-toolkit/build/bin/rt_register -m /Volumes/Working_4TB/IBW_no_Seam/optical/${ID}.jpg -f /Volumes/Working_4TB/IBW_no_Seam/no_seam_dataset/IBW_no_seam_frangi_gaussian_3/${ID}/obj/${ID}_enhanced.jpg -o /Volumes/Working_4TB/IBW_no_Seam/no_seam_dataset/IBW_no_seam_frangi_gaussian_3/${ID}/match/${ID}_reg.tif
+# echo "Registering for $ID (frangi)"
+# /Volumes/Working_4TB/utils/registration-toolkit/build/bin/rt_register -m /Volumes/Working_4TB/IBW_no_Seam/optical/${ID}.jpg -f /Volumes/Working_4TB/IBW_no_Seam/no_seam_dataset/IBW_no_seam_frangi/${ID}/obj/${ID}_enhanced.jpg -o /Volumes/Working_4TB/IBW_no_Seam/no_seam_dataset/IBW_no_seam_frangi/${ID}/match/${ID}_reg.tif
---
+++ data/scripts/run_register_batch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+data/scripts/register.sh --id 005
+data/scripts/register.sh --id 011
